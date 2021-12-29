00ffae7aa1010f34bd936fc23f3b3f7c0c2fd3a8
Adwaith S <adwaiths@live.in>
2021-12-29 22:45:03 +0530

    ADDED FILE UPLOAD FEATURE

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express=require("express");
+const fileUpload = require("express-fileupload")
 const app=express();
 const mongoose=require("mongoose");
 const dotenv=require("dotenv");
 const userRoute=require("./routes/user");
 const authRoute=require("./routes/auth");
 const summyRoute=require("./routes/summy");
+const {v1: uuidv1} = require('uuid');
+const { verifyTokenAuthazrization } = require("./routes/verifyToken");
 
+dotenv.config();
 const morgan = require('morgan')
 app.use(morgan('tiny'))
-dotenv.config();
 app.use(express.json());
+app.use(fileUpload({
+    useTempFiles : true,
+    tempFileDir : '/tmp/',
+    limits: { fileSize: 50 * 1024 * 1024 },
+}));
 mongoose.connect(process.env.MONGO_URL)
     .then(()=>console.log("DB CONNECTION SUCCCES"))
     .catch((err)=>console.log(err))
@@ -17,6 +25,18 @@ mongoose.connect(process.env.MONGO_URL)
 app.use("/user",userRoute);
 app.use("/auth",authRoute);
 app.use("/summy",summyRoute);
+
+app.post(
+    '/test/file-upload',verifyTokenAuthazrization,
+    (req, res) => {
+        if(req.files){
+            console.log(req.files, 'koko');
+            const filetemp=uuidv1();
+            req.files.file.mv('./data/'+filetemp)
+        }
+        res.send('ok')
+    }
+)
 
 
 app.listen(process.env.PORT || 3000,()=>{
