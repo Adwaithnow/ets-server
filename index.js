646ae4dcfd7e9f4404d655f1fcbfedcf93472443
Adwaith S <adwaiths@live.in>
2022-01-05 17:37:37 +0530

    Added cross access origin and allowed index.js

--- index.js
+++ index.js
@@ -16,6 +16,25 @@ dotenv.config();
 const morgan = require('morgan')
 app.use(morgan('tiny'))
 app.use(express.json());
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.use(fileUpload({
     useTempFiles : false,
     tempFileDir : '/tmp/',
